9f1aff7ed17c85304721ad0c1748ee696003e87c
Moaid Hashem <71054199+MoaidHashem3@users.noreply.github.com>
2024-08-25 14:07:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express")
 const mongoose = require("mongoose");
 const dbConn = require('./controllers/dbConn')
 const todoRouter = require('./routes/todoRoute')
+const userRouter = require('./routes/userRoute')
 const fs = require('fs')
 const app = express()
 
 app.use(express.json())
 app.use(todoRouter)
-
+app.use(userRouter)
 dbConn();
 
 
